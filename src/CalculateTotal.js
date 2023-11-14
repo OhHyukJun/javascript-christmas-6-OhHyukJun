8f99dc8b718146d95043aa3c1ca099664abc9b92
OhHyukJun <kmh7277@naver.com>
2023-11-14 13:50:24 +0900

    Refactor: 중복된 예외처리 삭제

--- src/CalculateTotal.js
+++ src/CalculateTotal.js
@@ -1,13 +1,10 @@
-import { menuBoard } from "./menuBoard";
+import { menuBoard } from "./Constants/menuBoard";
 
 export function calculateTotalPrice(menus) {
   let totalPrice = 0;
   menus.forEach(menu => {
     const menuName = menu[0];
     const menuCount = menu[1];
-    if (!menuBoard.hasOwnProperty(menuName)) {
-      throw new Error('[ERROR] 유효하지 않은 메뉴입니다.');
-    }
     totalPrice += menuBoard[menuName] * menuCount;
     });
   return totalPrice;
