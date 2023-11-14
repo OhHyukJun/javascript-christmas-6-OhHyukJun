be1ac29aaa027d478aa0bafc0ed6505ea9d55304
OhHyukJun <kmh7277@naver.com>
2023-11-14 17:45:21 +0900

    Test: 할인 테스트 구현 중

--- __tests__/DiscountTest.js
+++ __tests__/DiscountTest.js
@@ -7,20 +7,20 @@ import { WeekendDiscount } from "../src/Discount/WeekendDiscount";
 import { holidays } from "../src/Constants/calendar";
 
 describe("할인 테스트", () => {
-  test("DailyDiscount returns correct discount based on date", () => {
+  test("데일리 할인 테스트", () => {
     expect(DailyDiscount(1)).toEqual(1000);
     expect(DailyDiscount(10)).toEqual(1900);
     expect(DailyDiscount(30)).toEqual(0);
   });
 
-  test("WeekendDiscount returns correct discount based on date and menus", () => {
+  test("주중 할인 테스트", () => {
 
     const menus = [[`${menuCategories.메인[0]}`, 2], [`${menuCategories.메인[1]}`, 3], [`${menuCategories.메인[2]}`, 4]];
     expect(WeekendDiscount(holidays[1], menus)).toEqual({weekdayDiscount: 0, holidayDiscount: 0});
     expect(WeekendDiscount(holidays[2]+3, menus)).toEqual({weekdayDiscount: 6069, holidayDiscount: 0});
   });
 
-  test("GiveProduct returns correct gift based on totalPrice", () => {
+  test("증정 항목 테스트", () => {
     expect(GiveProduct(120000)).toEqual({product: `${GIFTS.CHAMPAGNE}`, productPrice: 25000});
     expect(GiveProduct(100000)).toEqual({product: `${GIFTS.NONE}`, productPrice: 0});
   });
