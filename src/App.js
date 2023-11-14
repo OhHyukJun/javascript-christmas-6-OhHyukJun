a9b74091f90759e7887e6be6d20ec35a5e5d14c4
OhHyukJun <kmh7277@naver.com>
2023-11-14 14:41:30 +0900

    Refactor: 메인 코드 리펙토링

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import InputView from "./InputView";
 import OutputView from "./OutputView";
-import { calculateTotalPrice } from "./CalculateTotal";
+import { CalculateTotalPrice } from "./CalculateTotal";
 import { MissionUtils } from "@woowacourse/mission-utils";
 import { GiveProduct } from "./Discount/GiveProduct";
-import { calculateTotalDiscount } from "./Discount/TotalDiscount";
-import { printDiscounts } from "./Discount/PrintDiscount";
+import { CalculateTotalDiscount } from "./Discount/TotalDiscount";
+import { PrintDiscounts } from "./Discount/PrintDiscount";
+import { ChristmasBadge } from "./ChristmasBadge";
 
 class App {
   constructor() {
@@ -19,21 +20,25 @@ class App {
       MissionUtils.Console.print(`${date}`);
       this.outputView.printMenu(menus);
 
-      const totalPrice = calculateTotalPrice(menus);
+      const totalPrice = CalculateTotalPrice(menus);
       MissionUtils.Console.print(`<할인 전 총주문 금액>\n${totalPrice}원`);
 
       const giveProduct = GiveProduct(totalPrice);
       MissionUtils.Console.print(`<증정 메뉴>\n${giveProduct.product}`);
 
-      //const printDiscount = printDiscounts(date,menus)
-      //MissionUtils.Console.print(`<혜택 내역>\n${printDiscount}`);
+      const printDiscount = PrintDiscounts(date,menus,totalPrice);
+      MissionUtils.Console.print(`<혜택 내역>\n${printDiscount}`);
 
-      const afterPrice = calculateTotalDiscount(date, menus);
+      const afterPrice = CalculateTotalDiscount(date, menus);
       MissionUtils.Console.print(`<총혜택 금액>\n${afterPrice.totalBenifit}원`);
       MissionUtils.Console.print(`<할인 후 예상 결제 금액>\n${afterPrice.afterPrice}원`);
+
+      const christmasBadge = ChristmasBadge(afterPrice);
+      MissionUtils.Console.print(`<12월 이벤트 배지>\n${christmasBadge}`);
     } catch (error) {
       if (error.message.startsWith("[ERROR]")) {
-        this.run(); 
+        MissionUtils.Console.print(error.message);
+        //this.run(); 
       } else {
         throw error;  
       }
