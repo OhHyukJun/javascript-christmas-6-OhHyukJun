db4ab443235aed1258567663a1e7bf2df3b68148
OhHyukJun <kmh7277@naver.com>
2023-11-14 11:05:41 +0900

    Feat: 주문 메뉴 출력

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,7 +1,13 @@
-export default OutputView = {
-    printMenu() {
-        Console.print("<주문 메뉴>");
-        // ...
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+const OutputView = {
+    printMenu(menus) {
+        MissionUtils.Console.print("<주문 메뉴>");
+        menus.forEach(menu => {
+            MissionUtils.Console.print(`${menu[0]} ${menu[1]}개\n`);
+        });
     }
-    // ...
+
 }
+
+export default OutputView;
