a4ffd05db30630c03a011a3d68453a1ec8e30ee9
OhHyukJun <kmh7277@naver.com>
2023-11-14 13:52:14 +0900

    Style: 공백 수정

--- src/Discount/DailyDiscount.js
+++ src/Discount/DailyDiscount.js
@@ -1,7 +1,8 @@
 export function DailyDiscount(date){
     let dailydiscount=0;
-    if(date<26)
+    if(date < 26)
         dailydiscount = 1000 + (date - 1) * 100;
+
     return dailydiscount;
 }
 
