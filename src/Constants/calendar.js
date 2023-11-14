42ab1cb508826cf3790ce36dc1e61219fa3b7119
OhHyukJun <kmh7277@naver.com>
2023-11-14 13:49:09 +0900

    Chore: 상수 추가 생성

---
+++ src/Constants/calendar.js
@@ -0,0 +1,3 @@
+export const holidays = [1,2,8,9,15,16,22,23,29,30];
+
+export const specialDays = [3,10,17,24,25,31];
---
+++ src/Constants/checkChampagne.js
@@ -0,0 +1,4 @@
+export const GIFTS = {
+    NONE: '없음',
+    CHAMPAGNE: '샴페인 1개'
+}
