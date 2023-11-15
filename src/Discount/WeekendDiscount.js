bc5a1905acec5942f571a4be3c27f40e4841d3dd
OhHyukJun <kmh7277@naver.com>
2023-11-15 14:04:53 +0900

    Refactor: 불필요한 코드 수정

--- src/Discount/WeekendDiscount.js
+++ src/Discount/WeekendDiscount.js
@@ -7,11 +7,11 @@ export function WeekendDiscount(date,menus){
     let weekdayDiscount=0,holidayDiscount=0;
     if(!holidays.includes(date))
         menus.forEach(menu => {
-            if(weekdayMenu.includes(menu[0])) weekdayDiscount += menu[1]*2023;
+            if(weekdayMenu.includes(menu[0])) weekdayDiscount = menu[1]*2023;
         });
     if(holidays.includes(date))
         menus.forEach(menu => {
-            if(holidayMenu.includes(menu[0])) holidayDiscount += menu[1]*2023;
+            if(holidayMenu.includes(menu[0])) holidayDiscount = menu[1]*2023;
         });
     return {weekdayDiscount,holidayDiscount};
 }
