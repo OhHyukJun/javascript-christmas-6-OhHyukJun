2a138a4a5c68088ed598f94baad94150b63d1495
OhHyukJun <kmh7277@naver.com>
2023-11-15 19:29:16 +0900

    Refactor: 혜택 내역 전체 출력하도록 코드 수정

--- src/Discount/PrintDiscount.js
+++ src/Discount/PrintDiscount.js
@@ -10,10 +10,10 @@ export function PrintDiscounts(date, menus, totalPrice) {
     const givePrice = GiveProduct(totalPrice);
 
     let discountDetails = '';
-    if(dailyDiscount > 0) discountDetails = `크리스마스 디데이 할인: -${dailyDiscount}원\n`;   
-    if(weekendDiscounts.weekdayDiscount > 0) discountDetails = `평일 할인: -${weekendDiscounts.weekdayDiscount}원\n`;
-    if(specialDiscount > 0) discountDetails = `특별 할인: -${specialDiscount}원\n`;
-    if(givePrice.productPrice > 0) discountDetails = `증정 이벤트: -${givePrice.productPrice}원\n`;
+    if(dailyDiscount > 0) discountDetails += `크리스마스 디데이 할인: -${dailyDiscount}원\n`;   
+    if(weekendDiscounts.weekdayDiscount > 0) discountDetails += `평일 할인: -${weekendDiscounts.weekdayDiscount}원\n`;
+    if(specialDiscount > 0) discountDetails += `특별 할인: -${specialDiscount}원\n`;
+    if(givePrice.productPrice > 0) discountDetails += `증정 이벤트: -${givePrice.productPrice}원\n`;
     if(discountDetails === '') discountDetails = '없음';
     return discountDetails;
 }
